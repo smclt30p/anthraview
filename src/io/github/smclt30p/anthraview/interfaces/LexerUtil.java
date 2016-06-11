6483c6457d29577c6fc96187cb88a1957aa1ac3d
Ognjen Galic <smclt30p@gmail.com>
2016-06-11 15:09:50 +0200

    Implemented file lexing

---
+++ src/io/github/smclt30p/anthraview/interfaces/LexerUtil.java
@@ -0,0 +1,7 @@
+package io.github.smclt30p.anthraview.interfaces;
+
+public interface LexerUtil {
+    boolean goForward();
+    boolean goBack();
+    String getChar();
+}
---
+++ src/io/github/smclt30p/anthraview/reader/Lexer.java
@@ -0,0 +1,39 @@
+package io.github.smclt30p.anthraview.reader;
+
+import io.github.smclt30p.anthraview.interfaces.LexerUtil;
+
+public class Lexer implements LexerUtil {
+
+    private int index;
+    private String[] fileChars;
+
+    public Lexer(String[] fileChars) {
+        this.fileChars = fileChars;
+        this.index = -1;
+    }
+
+    @Override
+    public boolean goForward() {
+        int operator = index;
+        if ((++operator) > this.fileChars.length - 1) {
+            return false;
+        }
+        index = operator;
+        return true;
+    }
+
+    @Override
+    public boolean goBack() {
+        int operator = index;
+        if (operator < 0) {
+            return false;
+        }
+        index--;
+        return true;
+    }
+
+    @Override
+    public String getChar() {
+        return fileChars[index];
+    }
+}
