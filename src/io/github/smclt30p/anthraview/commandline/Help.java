5b6ea48493a715414caedbb0d94ebc3e7b19bbd3
Ognjen Galic <smclt30p@gmail.com>
2016-06-13 13:51:50 +0200

    Update help and version info

--- src/io/github/smclt30p/anthraview/commandline/Help.java
+++ src/io/github/smclt30p/anthraview/commandline/Help.java
@@ -6,15 +6,15 @@ public abstract class Help {
             "View Anthrazit Logs, format then and convert to human readable formats.\n\n" +
             "\t--color - view logs in color (Supported on bash or zsh systems only)\n" +
             "\t--timestamp - view logs with human readable timestamps\n" +
-            "\t--dmesg - view log entries age in miliseconds from start";
+            "\t--dmesg - view log entries age in miliseconds from start\n";
 
-    private static final String VERSION = "anthraview (Anthrazit) 1.0 build 204\n" +
+    private static final String VERSION = "anthraview (Anthrazit) 1.0 build 205\n" +
             "Copyright (C) 2016 Free Time Foundation, Inc.\n" +
-            "License GPLv3+: GNU GPL version 2 <http://gnu.org/licenses/gpl.html>.\n" +
+            "License GPLv2: GNU GPL version 2 <http://gnu.org/licenses/gpl.html>.\n" +
             "This is free software: you are free to change and redistribute it.\n" +
             "There is NO WARRANTY, to the extent permitted by law.\n" +
             "\n" +
-            "Written by Ognjen Galić.";
+            "Written by Ognjen Galić.\n";
 
     public static void printHelp() {
         System.out.print(HELP);
