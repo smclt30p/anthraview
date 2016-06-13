c92d3cc11e72d0f3a0b6d6415d3ee03f889ee25e
Ognjen Galic <smclt30p@gmail.com>
2016-06-13 23:33:59 +0200

    First working example - full

--- src/io/github/smclt30p/anthraview/Main.java
+++ src/io/github/smclt30p/anthraview/Main.java
@@ -3,6 +3,12 @@ package io.github.smclt30p.anthraview;
 import io.github.smclt30p.anthraview.commandline.Help;
 import io.github.smclt30p.anthraview.commandline.Parameter;
 import io.github.smclt30p.anthraview.commandline.ParameterParser;
+import io.github.smclt30p.anthraview.printer.DmesgPrinter;
+import io.github.smclt30p.anthraview.printer.HumanPrinter;
+import io.github.smclt30p.anthraview.printer.Printer;
+import io.github.smclt30p.anthraview.printer.StandardPrinter;
+
+import java.io.File;
 
 public class Main {
 
@@ -12,14 +18,31 @@ public class Main {
             Help.printHelp();
         }
 
-        Parameter[] params = ParameterParser.parse(args);
-        for (Parameter p : params) {
-            if (p.getArgument().equals("help")) {
-                Help.printHelp();
-            } else if (p.getArgument().equals("version")) {
-                Help.printVersion();
-            }
+        Parameter[] p = ParameterParser.parse(args);
+
+        if (ParameterParser.checkForParameter(p, "version")) {
+            Help.printVersion();
+        } else if (ParameterParser.checkForParameter(p, "help")) {
+            Help.printHelp();
         }
+
+        File log = new File(p[0].getArgument());
+        Printer pr;
+
+        if (ParameterParser.checkForParameter(p, "dmesg")) {
+            pr = new DmesgPrinter(log);
+        } else if (ParameterParser.checkForParameter(p, "human")) {
+            pr = new HumanPrinter(log);
+        } else {
+            pr = new StandardPrinter(log);
+        }
+
+        if (ParameterParser.checkForParameter(p, "color")) {
+            pr.printColor();
+        } else {
+            pr.print();
+        }
+
     }
 }
 
