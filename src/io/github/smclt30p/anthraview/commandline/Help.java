e4e06417d82684b06d4b6abfe8cc427ea5c51168
Ognjen Galic <smclt30p@gmail.com>
2016-06-13 13:45:18 +0200

    Implement Version and Help in the command line

---
+++ src/io/github/smclt30p/anthraview/commandline/Help.java
@@ -0,0 +1,32 @@
+package io.github.smclt30p.anthraview.commandline;
+
+public abstract class Help {
+
+    private static final String HELP = "Usage: anthraview --[ARGUMENT]=[PARAMETER] -- ...\n\n" +
+            "View Anthrazit Logs, format then and convert to human readable formats.\n\n" +
+            "\t--color - view logs in color (Supported on bash or zsh systems only)\n" +
+            "\t--timestamp - view logs with human readable timestamps\n" +
+            "\t--dmesg - view log entries age in miliseconds from start";
+
+    private static final String VERSION = "anthraview (Anthrazit) 1.0\n" +
+            "Copyright (C) 2016 Free Time Foundation, Inc.\n" +
+            "License GPLv3+: GNU GPL version 2 <http://gnu.org/licenses/gpl.html>.\n" +
+            "This is free software: you are free to change and redistribute it.\n" +
+            "There is NO WARRANTY, to the extent permitted by law.\n" +
+            "\n" +
+            "Written by Ognjen Galić.";
+
+    public static void printHelp() {
+        System.out.print(HELP);
+        System.exit(0);
+    }
+
+    public static void printVersion() {
+        System.out.print(VERSION);
+        System.exit(0);
+    }
+
+}
+
+
+
